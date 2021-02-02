358e0125cdd1de65e6f78cfeeb66667ec1174821
de Witte <Benoit.DeWitte@student.uliege.be>
2021-02-02 20:26:27 +0100

    Add Player Class

--- batailleLikeGame.py
+++ batailleLikeGame.py
@@ -7,24 +7,57 @@ class MainWindow(QMainWindow):
     '''Stupid 'bataille' like game '''
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('First signals and slots')
+        self.setWindowTitle('Battle cards!')
         self.checkButton = checkButton(self, 'Check Winner')
 
-        self.player1 = QLabel(str(randint(1,10)))
-        self.player2 = QLabel(str(randint(1,10)))
 
         self.vLayout = QVBoxLayout()
-        self.firstRow = QHBoxLayout()
-
-        self.firstRow.addWidget(self.player1)
-        self.firstRow.addWidget(self.player2)
-
+        self.players = Players()
+        self.firstRow = self.players.container
+        
         self.vLayout.addLayout(self.firstRow)
         self.vLayout.addWidget(self.checkButton)
 
         self.grid = QWidget()
         self.grid.setLayout(self.vLayout)
         self.setCentralWidget(self.grid)
+
+class Players():
+    def __init__(self):
+        self.container = QHBoxLayout()
+        self.player1 = Player('Ben')
+        self.player2 = Player("Manu")
+        self.container.addLayout(self.player1.container)
+        self.container.addLayout(self.player2.container)
+    
+    def shuffle_cards(self):
+        self.player1.set_value()
+        self.player2.set_value()
+    
+    def compare_cards(self):
+        if self.player1.value > self.player2.value:
+            print("{} Won ! ({} vs {})".format(self.player1.name,
+                self.player1.value, self.player2.value))
+        elif self.player1.value < self.player2.value:
+            print("{} Won ! ({} vs {})".format(self.player2.name,
+                                              self.player1.value, self.player2.value))
+        else : 
+            print("EQUALITY")
+
+class Player():
+    def __init__(self, name):
+        self.container = QVBoxLayout()
+        self.name = name
+        self.nameLabel = QLabel(name)
+
+        self.label = QLabel()
+        self.set_value()
+    
+        self.container.addWidget(self.nameLabel)
+        self.container.addWidget(self.label)
+    def set_value(self):
+        self.value = randint(1,10) # Changer ça
+        self.label.setText(str(self.value))
 
 class checkButton(QPushButton):
     def __init__(self, root, *args):
@@ -35,16 +68,10 @@ class checkButton(QPushButton):
 
     def check_winner(self):
         '''Check which player win the game'''
-        if int(self.root.player1.text()) > int(self.root.player2.text()):
-            print("Player 1 Won ! ({} vs {})".format(
-                int(self.root.player1.text()), int(self.root.player2.text())))
-        else : 
-            print("Player 2 Won !({} vs {})".format(
-                int(self.root.player1.text()), int(self.root.player2.text())))
+        self.root.players.compare_cards()
     def shuffle(self):
         '''Reset value of player'''
-        self.root.player1.setText(str(randint(1,10)))
-        self.root.player2.setText(str(randint(1,10)))
+        self.root.players.shuffle_cards()
 
 app = QApplication(sys.argv)
 window = MainWindow()
